fb76ea0fcab5952546e3a83bdf63b340868174aa
Bruno Monteiro <brunomourabastos@gmail.com>
2022-08-22 14:39:52 -0300

    fix: Subindo comigo pela raiz

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -12,7 +12,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Testar rota /login', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
---
+++ app/backend/src/tests/mocks/userMock.ts
@@ -0,0 +1,5 @@
+import { IUser }from '../../database/interfaces/IUser';
+
+export default const userMock: IUser {
+
+}
